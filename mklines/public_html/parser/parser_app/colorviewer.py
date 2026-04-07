c354974c769620db69425cde012b87f6e60f3f7f
MaxVor1992 <autozona54@mail.ru>
2026-04-07 21:50:57 +0300

    12

--- mklines/public_html/parser/parser_app/colorviewer.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-import parser_utils
-
-root = tkinter.Tk()
-for name,color in parser_utils.colors.items():
-    tkinter.Label(root,text=name, bg=color).pack(fill="x")
-
-root.mainloop()
--- mklines/public_html/parser/parser_app/regions.py
+++
@@ -1,7 +0,0 @@
-import json
-region_y = map(lambda x: x.strip().split(","), open('lrs.txt').readlines())
-region_g = json.load(open('geo.json'))
-
-print(region_g)
-print(type(region_g))
-
